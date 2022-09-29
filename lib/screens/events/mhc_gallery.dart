121623de695613d3b600c3e678576f4bd60280bf
Eugene Asiedu <uginasiedu079@gmail.com>
2022-09-29 20:21:06 +0000

    fixed mhc gallery page

--- lib/screens/events/mhc_gallery.dart
+++ lib/screens/events/mhc_gallery.dart
@@ -55,37 +55,74 @@ class MHCGallery extends StatelessWidget {
             ),
           ),
         ),
-        body: SingleChildScrollView(
-          child: Container(
-            child: Padding(
-              padding:
-                  const EdgeInsets.symmetric(horizontal: 20.0, vertical: 34.0),
-              child: GridView.builder(
-                shrinkWrap: true,
-                itemCount: imageFileLocations.length,
-                physics: const NeverScrollableScrollPhysics(),
-                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 3,
-                    crossAxisSpacing: 7,
-                    mainAxisSpacing: 7,
-                    mainAxisExtent: height * 0.1079),
-                itemBuilder: (BuildContext context, int index) {
-                  return Container(
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(8.0),
-                    ),
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.circular(8.0),
-                      child: CommonImageView(
-                        imagePath: imageFileLocations[index],
-                        fit: BoxFit.fill,
+        body: Stack(
+          children: [
+            SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(
+                    horizontal: 20.0, vertical: 34.0),
+                child: GridView.builder(
+                  shrinkWrap: true,
+                  itemCount: imageFileLocations.length,
+                  physics: const NeverScrollableScrollPhysics(),
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 3,
+                      crossAxisSpacing: 7,
+                      mainAxisSpacing: 7,
+                      mainAxisExtent: height * 0.1079),
+                  itemBuilder: (BuildContext context, int index) {
+                    return Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(8.0),
                       ),
-                    ),
-                  );
-                },
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(8.0),
+                        child: CommonImageView(
+                          imagePath: imageFileLocations[index],
+                          fit: BoxFit.fill,
+                        ),
+                      ),
+                    );
+                  },
+                ),
               ),
             ),
-          ),
+            Positioned(
+              top: height * 0.768,
+              left: 0,
+              right: 0,
+              bottom: height * 0.0467,
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 24),
+                child: Container(
+                  child: Row(
+                    children: [
+                      Expanded(
+                        flex: 3,
+                        child: Button(
+                          text: "lbl_back".tr..toUpperCase(),
+                          onPressed: () {},
+                          textColor: Constants.lightBlue700,
+                          buttonColor: Constants.lightBlue50,
+                        ),
+                      ),
+                      SizedBox(
+                        width: 16,
+                      ),
+                      Expanded(
+                        flex: 6,
+                        child: Button(
+                          text: "lbl_set_cover".tr.toUpperCase(),
+                          onPressed: () {},
+                          textColor: Colors.white,
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );

55bc8fc477fb940742661261be99cf8681f808c3
David Brewu <david.brewu@ashesi.edu.gh>
2022-10-03 04:38:10 +0000

    completed general info pages

---
+++ lib/screens/events/filter_controller.dart
@@ -0,0 +1,8 @@
+import '../../util/exports.dart';
+
+class FilterController extends GetxController{
+  
+  var sortBy = "Individual Rate: Lowest First".tr.obs;
+  var rating = "All".obs;
+  var location = "All".obs;
+}
